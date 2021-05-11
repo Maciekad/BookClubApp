868112e4d7e7f292cd8a4b6c88065784ac8e26a2
Maciej Adrych <m.adrych@gmail.com>
2021-05-11 12:57:58 +0200

    Dodanie footera do komponentu HomePage

---
+++ src/Components/Video.jsx
@@ -0,0 +1,14 @@
+
+const Video = () => {
+    return(
+        <div className="shadow-sm mt-5 p-5 bg-green">
+            <div className="col-12 text-center text-light">
+                <h2 className="">Zobacz jakie to proste!</h2>
+                <h5 className="font-weight-light">Sprawdź jak zacząć zabawę z Book Club </h5>
+                <img className="play border-dotted mt-3 p-3" src="images/play.JPG" />
+            </div>
+        </div>
+    );
+}
+
+export default Video;
