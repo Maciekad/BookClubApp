8acbe26cced99f5472f30b4a79cb1582927add72
Maciej Adrych <m.adrych@gmail.com>
2021-05-11 12:34:52 +0200

    Dodanie obrazków do strony

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navigation from './Components/Navigation';
 import Footer from './Components/Footer';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <HomePage/>
       <Footer/>
     </div>
   );
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,13 @@
+import Header from './Header';
+import Navigation from './Navigation';
 
 const HomePage = () => {
     return(
-        
+        <div>
+            <Header/>
+            <Navigation/>
+        </div>
     );
 }
+
+export default HomePage;
